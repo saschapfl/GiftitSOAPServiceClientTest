a3884c664eb080e4ab0d26a6d1eee08d21dd12dc
Sven <sven@zelinka.de>
2019-04-04 09:59:13 +0200

    Client weiterbauen

--- src/main/java/dhbwka/wwi/vertsys/ws/soap_client/Main.java
+++ src/main/java/dhbwka/wwi/vertsys/ws/soap_client/Main.java
@@ -15,6 +15,10 @@ import dhbwka.wwi.vertsys.javaee.giftit.soap.GiftGiftit;
 import dhbwka.wwi.vertsys.javaee.giftit.soap.CategoryGiftit;
 import dhbwka.wwi.vertsys.javaee.giftit.soap.GiftItService;
 import dhbwka.wwi.vertsys.javaee.giftit.soap.InvalidCredentialsException_Exception;
+import dhbwka.wwi.vertsys.javaee.giftit.soap.UserAlreadyExistsException_Exception;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
@@ -23,41 +27,75 @@ import java.util.List;
  */
 public class Main {
 
-    public static void main(String[] args) throws InvalidCredentialsException_Exception {
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws InvalidCredentialsException_Exception, IOException, UserAlreadyExistsException_Exception {
         // Stub-Objekt zum entfernten Aufruf erstellen
         GiftItService service = new GiftItService();
         GiftIt giftWs = service.getGiftItPort();
 
-        // Webservice-Operation "findAll" aufrufen
-        try {
-            List<GiftGiftit> allGifts = giftWs.findallgifts("viktoria", "viktoria");
-            // Abgerufenes Ergebnis anzeigen
-            System.out.println("========================");
-            System.out.println("Die große SOAP-Geschenkideenliste");
-            System.out.println("========================");
-            System.out.println();
+        // Benutzerdaten einlesen
+        System.out.println("Registrierungsdaten:");
+        System.out.println("Vorname: ");
+        String firstName = in.readLine();
+        System.out.println("Nachname: ");
+        String lastName = in.readLine();
+        System.out.println("Benutzername: ");
+        String userName = in.readLine();
+        System.out.println("Passwort: ");
+        String password = in.readLine();
 
-            for (GiftGiftit gift : allGifts) {
-                System.out.println("Text:         " + gift.getLongText());
-                System.out.println("Kategorie: " + gift.getCategory());
-                System.out.println("Ersteller:         " + gift.getOwner());
+        try {
+            String status = giftWs.signup(userName, password, firstName, lastName);
+
+            //Wenn status OK --> go!
+            if (status.equals("OK")) {
+
+                // Webservice-Operation "findAll" aufrufen
+                List<GiftGiftit> allGifts = giftWs.findallgifts(userName, password);
+                // Abgerufenes Ergebnis anzeigen
+                System.out.println("========================");
+                System.out.println("Die große SOAP-Geschenkideenliste");
+                System.out.println("========================");
                 System.out.println();
+
+                for (GiftGiftit gift : allGifts) {
+                    System.out.println("Text:         " + gift.getLongText());
+                    System.out.println("Kategorie: " + gift.getCategory());
+                    System.out.println("Ersteller:         " + gift.getOwner());
+                    System.out.println();
+                }
+
+                System.out.print("Zu welchem User möchten Sie Geschenke sehen?");
+                String userToSearch = in.readLine();
+
+                List<GiftGiftit> filteredGifts = giftWs.findByUsername(userName, password, userToSearch);
+                System.out.println("========================");
+                System.out.println("Viktorias Geschenke");
+                System.out.println("========================");
+                System.out.println();
+
+                for (GiftGiftit gift : filteredGifts) {
+                    System.out.println("Text:         " + gift.getLongText());
+                    System.out.println("Kategorie: " + gift.getCategory());
+                    System.out.println("Ersteller:         " + gift.getOwner());
+                    System.out.println();
+                }
+
+                System.out.print("Kategorie: ");
+                String category = in.readLine();
+                System.out.print("Geschenkstatus: ");
+                String status_gift = in.readLine();
+                System.out.print("Suchbegriff: ");
+                String search = in.readLine();
             }
         } catch (InvalidCredentialsException_Exception ice) {
             System.out.println("Ungültige Benutzerdaten!");
+        } catch (UserAlreadyExistsException_Exception uae) {
+            System.out.println("Der Username ist bereits vorhanden");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage().getClass());
         }
 
-        List<GiftGiftit> filteredGifts = giftWs.findByUsername("viktoria", "viktoria", "viktoria");
-        System.out.println("========================");
-        System.out.println("Viktorias Geschenke");
-        System.out.println("========================");
-        System.out.println();
-
-        for (GiftGiftit gift : filteredGifts) {
-            System.out.println("Text:         " + gift.getLongText());
-            System.out.println("Kategorie: " + gift.getCategory());
-            System.out.println("Ersteller:         " + gift.getOwner());
-            System.out.println();
-        }
     }
 }
